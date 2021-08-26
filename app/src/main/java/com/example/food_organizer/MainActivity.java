abc28109ae50b7bfb3d08fe78e5dfe27fdd24dcd
Jaswanth <cs19b042@iittp.ac.in>
2021-08-26 23:30:28 +0530

    update database

--- app/src/main/java/com/example/food_organizer/MainActivity.java
+++ app/src/main/java/com/example/food_organizer/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(MainActivity.this,"Logged in succesfully",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),HomePage.class));
+                            startActivity(new Intent(MainActivity.this,HomePage.class));
                         }
                         else{
                             Toast.makeText(MainActivity.this,"Error"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
