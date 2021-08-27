d214d5e29d99fe0628933eeb0782a6e9c232c7c8
pavan <karanamkpk@gmail.com>
2021-08-27 13:08:49 +0530

    home page updated

---
+++ app/src/main/java/com/example/food_organizer/FragmentAdapter.java
@@ -0,0 +1,34 @@
+package com.example.food_organizer;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+
+
+    public FragmentAdapter(FragmentManager fm, Lifecycle lifecycle) {
+        super(fm,lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position)
+        {
+            case 1:
+                return new HomeFragment();
+            case 2:
+                return new ProfileFragment();
+        }
+        return new InventoryFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
