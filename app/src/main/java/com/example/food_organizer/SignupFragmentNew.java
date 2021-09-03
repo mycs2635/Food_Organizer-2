3c54c5532bbbedb50ef1617d55c679a8ae2d4e48
Jaswanth <cs19b042@iittp.ac.in>
2021-09-03 20:26:43 +0530

    no changes

--- app/src/main/java/com/example/food_organizer/SignupFragmentNew.java
+++ app/src/main/java/com/example/food_organizer/SignupFragmentNew.java
@@ -1,12 +1,25 @@
 package com.example.food_organizer;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +28,15 @@ import android.view.ViewGroup;
  */
 public class SignupFragmentNew extends Fragment {
 
+
+    private Button crtProfile;
+    EditText name,gender,phone,mail,userName,password,cPassword;
+    CheckBox tick;
+
+    FirebaseAuth mAuth;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +81,96 @@ public class SignupFragmentNew extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_new, container, false);
+        View v = inflater.inflate(R.layout.fragment_signup_new, container, false);
+        crtProfile = v.findViewById(R.id.bt_createprofile);
+
+        crtProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHelper obj=new databaseHelper(getContext());
+                if(checkAllTextFields()){
+
+                    rootNode = FirebaseDatabase.getInstance();
+                    reference = rootNode.getReference("users");
+                    Customer newProfile=new Customer(name.getText().toString(),
+                            gender.getText().toString(),
+                            phone.getText().toString(),
+                            mail.getText().toString(),
+                            userName.getText().toString(),
+                            password.getText().toString());
+
+//                    Intent verify = new Intent(ProfilePage.this,PhoneNoVerification.class);
+//                    verify.putExtra("phoneNo",phone.getText().toString());
+//                    startActivity(verify);
+
+
+                    reference.child(phone.getText().toString()).setValue(newProfile);
+                    mAuth.createUserWithEmailAndPassword(mail.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getContext(),"User created successfully",Toast.LENGTH_SHORT).show();
+                                Intent gotoHome = new Intent(getContext(),HomePage.class);
+                                startActivity(gotoHome);
+                                getActivity().finish();
+                            }
+                        }
+                    });
+                    // Toast.makeText(ProfilePage.this, newProfile.toString(), Toast.LENGTH_SHORT).show();
+
+                    obj.addUser(newProfile);
+                    Toast.makeText(getContext(),"creating profile", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        return v;
+    }
+
+    private boolean checkAllTextFields(){
+        if(name.length() == 0){
+            name.setError("This field is required");
+            return false;
+        }
+        if(gender.length() == 0){
+            gender.setError("This field is required");
+            return false;
+        }
+        if(phone.length() == 0){
+            phone.setError("This field is required");
+            return false;
+        }
+        if(mail.length() == 0){
+            mail.setError("This field is required");
+            return false;
+        }
+        if(userName.length() == 0){
+            userName.setError("This field is required");
+            return false;
+        }
+        if(password.length() == 0){
+            password.setError("This field is required");
+            return false;
+        }
+        else if(password.length() < 6){
+            password.setError("password length should be at least 8");
+            return false;
+        }
+        if(cPassword.length() == 0){
+            cPassword.setError("This field is required");
+            return false;
+        }
+        else if (!cPassword.getText().toString().equals(password.getText().toString())){
+            cPassword.setError("This field should be matched with password");
+            return false;
+        }
+
+//        if(!tick.isActivated()){
+//            tick.setError("This field is required");
+//            return false;
+//        }
+
+        return true;
     }
 }
